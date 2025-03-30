e3891aff3031424cf2721cb225bf35df990dbbec
Muthu Kumaran <mkumaran@xolta.com>
2025-03-30 21:48:55 +0200

    basic array method

--- src/components/nav/nav-tree.tsx
+++ src/components/nav/nav-tree.tsx
@@ -22,7 +22,19 @@ const slideNav: TreeLink[] = [
   { id: "2", title: "creating", url: "/arrays/02-creating" },
   { id: "3", title: "prop", url: "/arrays/03-prop" },
   { id: "4", title: "access", url: "/arrays/04-access-ele" },
-  //{ id: "30", title: "parent", children: [{ id: "30.1", title: "ch 1", url: "/arrays/03-mdonly" }] },
+  {
+    id: "5",
+    title: "basic array methods",
+    children: [
+      { id: "5.1", title: "push() and pop()", url: "/arrays/methods/push-pop" },
+      { id: "5.2", title: "unshift() and shift()", url: "/arrays/methods/unshift-shift" },
+      { id: "5.3", title: "concat()", url: "/arrays/methods/concat" },
+      { id: "5.4", title: "slice()", url: "/arrays/methods/slice" },
+      { id: "5.5", title: "indexOf() and includes()", url: "/arrays/methods/indexOf-includes" },
+      { id: "5.6", title: "join()", url: "/arrays/methods/join" },
+    ],
+  },
+  { id: "6", title: "passing-func", url: "/arrays/05-passing-fun" },
 ];
 
 interface props {
